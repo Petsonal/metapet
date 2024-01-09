d90d970474093d185a8b31da34c79bdf23629823
Hoang Minh Thanh <hmthanhgm@gmail.com>
2024-01-09 17:47:22 +0700

    add url

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
 import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
+import { Code } from "../components/code";
+import { Card } from "../components/card";
+import { Button } from "../components/button";
+// import { Code } from "@repo/ui/code";
+// import { Card } from "@repo/ui/card";
+// import { Button } from "@repo/ui/button";
 
 function Gradient({
   conic,

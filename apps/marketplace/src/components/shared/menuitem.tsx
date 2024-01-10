5606a1faf2d9ce525e33be39d0c18a979c9f9dba
Hoang Minh Thanh <hmthanhgm@gmail.com>
2024-01-10 20:24:09 +0700

    update all

---
+++ apps/marketplace/src/components/shared/menuitem.tsx
@@ -0,0 +1,97 @@
+export default function MenuItem() {
+  return (
+    <div className="mt-6 flow-root">
+      <div className="-my-6 divide-y divide-gray-500/10">
+        <div className="space-y-2 py-6">
+          <div className="-mx-3">
+            <button
+              type="button"
+              className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+              aria-controls="disclosure-1"
+              aria-expanded="false"
+            >
+              Product
+              <svg className="h-5 w-5 flex-none" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path
+                  fill-rule="evenodd"
+                  d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </button>
+            <div className="mt-2 space-y-2" id="disclosure-1">
+              <a
+                href="#"
+                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+              >
+                Analytics
+              </a>
+              <a
+                href="#"
+                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+              >
+                Engagement
+              </a>
+              <a
+                href="#"
+                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+              >
+                Security
+              </a>
+              <a
+                href="#"
+                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+              >
+                Integrations
+              </a>
+              <a
+                href="#"
+                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+              >
+                Automations
+              </a>
+              <a
+                href="#"
+                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+              >
+                Watch demo
+              </a>
+              <a
+                href="#"
+                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+              >
+                Contact sales
+              </a>
+            </div>
+          </div>
+          <a
+            href="#"
+            className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+          >
+            Features
+          </a>
+          <a
+            href="#"
+            className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+          >
+            Marketplace
+          </a>
+          <a
+            href="#"
+            className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+          >
+            Company
+          </a>
+        </div>
+        <div className="py-6">
+          <a
+            href="#"
+            className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+          >
+            Log in
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
--- apps/marketplace/src/pages/_document.tsx
+++ apps/marketplace/src/pages/_document.tsx
@@ -1,11 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Footer from "@/components/shared/footer"
+import Header from "@/components/shared/header"
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="h-full chakra-ui-light">
+        <Header />
         <Main />
+        <Footer />
         <NextScript />
       </body>
     </Html>

ad1f121d6418165a66c05f1b2f4acf3409b73bca
Hoang Minh Thanh <hmthanhgm@gmail.com>
2024-01-10 23:40:13 +0700

    update menu item

--- apps/web/src/components/shared/menuitem.tsx
+++ apps/web/src/components/shared/menuitem.tsx
@@ -1,97 +1,23 @@
+import Link from "next/link"
+
 export default function MenuItem() {
   return (
-    <div className="mt-6 flow-root">
-      <div className="-my-6 divide-y divide-gray-500/10">
-        <div className="space-y-2 py-6">
-          <div className="-mx-3">
-            <button
-              type="button"
-              className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-              aria-controls="disclosure-1"
-              aria-expanded="false"
-            >
-              Product
-              <svg className="h-5 w-5 flex-none" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path
-                  fillRule="evenodd"
-                  d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </button>
-            <div className="mt-2 space-y-2" id="disclosure-1">
-              <a
-                href="#"
-                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-              >
-                Analytics
-              </a>
-              <a
-                href="#"
-                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-              >
-                Engagement
-              </a>
-              <a
-                href="#"
-                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-              >
-                Security
-              </a>
-              <a
-                href="#"
-                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-              >
-                Integrations
-              </a>
-              <a
-                href="#"
-                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-              >
-                Automations
-              </a>
-              <a
-                href="#"
-                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-              >
-                Watch demo
-              </a>
-              <a
-                href="#"
-                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-              >
-                Contact sales
-              </a>
-            </div>
-          </div>
-          <a
-            href="#"
-            className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-          >
-            Features
-          </a>
-          <a
-            href="#"
-            className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-          >
-            Marketplace
-          </a>
-          <a
-            href="#"
-            className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-          >
-            Company
-          </a>
-        </div>
-        <div className="py-6">
-          <a
-            href="#"
-            className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-          >
-            Log in
-          </a>
-        </div>
-      </div>
+    <div className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-4">
+      <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+        <li className="block py-2 pr-4 pl-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+          <Link href="/">Home</Link>
+        </li>
+
+        <li className="block py-2 pr-4 pl-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+          <Link href="/market">Market</Link>
+        </li>
+        <li className="block py-2 pr-4 pl-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+          <Link href="/create">Create</Link>
+        </li>
+        <li className="block py-2 pr-4 pl-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+          <Link href="/about">About</Link>
+        </li>
+      </ul>
     </div>
   )
 }

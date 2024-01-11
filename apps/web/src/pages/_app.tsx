3299b1c22ab5e35f8e3a39fecfa4982020876247
Hoang Minh Thanh <hmthanhgm@gmail.com>
2024-01-11 20:06:42 +0700

    update

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -4,6 +4,19 @@ import "@/styles/normalize.css"
 import type { AppProps } from "next/app"
 import { ChakraProvider } from "@chakra-ui/react"
 import theme from "@/lib/theme"
+import { ConnectKitProvider, getDefaultConfig } from 'connectkit';
+
+import { createConfig, http } from 'wagmi'
+import { mainnet, sepolia } from 'wagmi/chains'
+
+const config = createConfig({ 
+  chains: [mainnet, sepolia],
+  ssr: true, 
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+  },
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (

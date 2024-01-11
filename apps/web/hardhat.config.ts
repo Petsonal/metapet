f9ef4bb7c2dcb3e1df10389781364fee7143a067
Hoang Minh Thanh <hmthanhgm@gmail.com>
2024-01-11 14:59:13 +0700

    update

--- apps/web/hardhat.config.ts
+++ apps/web/hardhat.config.ts
@@ -1,15 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config"
-import "@nomicfoundation/hardhat-toolbox-viem"
+// import "@nomicfoundation/hardhat-toolbox-viem"
+import "@nomicfoundation/hardhat-toolbox"
+import { config as dotEnvConfig } from "dotenv"
+dotEnvConfig()
 
-
+console.log("PRIVATE_KEY", process.env.PRIVATE_KEY)
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
   networks: {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [process.env.PRIVATE_KEY || ""]
-    }
-  }
+      accounts: ["29c7b718cec0dad617742acf1c0dbda9b0b57ab9dc002c1ed643167968f6be46"],
+    },
+  },
 }
 
 export default config

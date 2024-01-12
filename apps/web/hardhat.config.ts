4bdede8b1c37754ea1b0c23fb1cc3f5af0ef6b18
Hoang Minh Thanh <hmthanhgm@gmail.com>
2024-01-12 16:49:10 +0700

    update markplace

--- apps/web/hardhat.config.ts
+++ apps/web/hardhat.config.ts
@@ -1,14 +1,28 @@
 import { HardhatUserConfig } from "hardhat/config"
-import "@nomicfoundation/hardhat-toolbox-viem"
 import { config as dotEnvConfig } from "dotenv"
+// import "@nomiclabs/hardhat-waffle";
+// import "@nomicfoundation/hardhat-toolbox-viem"
+require("@nomicfoundation/hardhat-toolbox-viem")
 dotEnvConfig()
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  defaultNetwork: "hardhat",
   networks: {
-    sepolia: {
-      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: ["29c7b718cec0dad617742acf1c0dbda9b0b57ab9dc002c1ed643167968f6be46"],
+    hardhat: {
+      chainId: 1337,
+    },
+    // sepolia: {
+    //   url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    //   accounts: ["29c7b718cec0dad617742acf1c0dbda9b0b57ab9dc002c1ed643167968f6be46"],
+    // },
+  },
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
     },
   },
 }
